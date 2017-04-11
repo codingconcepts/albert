454c672e6ea1baabd0c7aa2619bc2a0b32bdd928
codingconcepts <bassrob@gmail.com>
2017-04-11 19:51:48 +0100

    Using NewAgentFromConfig for agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,35 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/codingconcepts/albert/pkg/agent"
-	"github.com/codingconcepts/albert/pkg/model"
-	nats "github.com/nats-io/go-nats"
 )
 
 func main() {
-	config, err := agent.NewConfigFromFile("config.json")
+	a, err := agent.NewAgentFromConfig("config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	logger := model.NewLogger(os.Stdout, config.LogLevel.Level)
-
-	opts := nats.Options{
-		User:     config.NatsUser,
-		Password: config.NatsPass,
-		Servers:  config.NatsHosts,
-	}
-
-	conn, err := opts.Connect()
-	if err != nil {
-		logger.Fatal(err)
-	}
-
-	a := agent.NewAgent(config, conn, logger)
 	go a.Start()
 
-	logger.Info("agent started successfully")
+	a.Logger.Info("agent started successfully")
 	fmt.Scanln()
+
+	a.Stop()
 }
